0e58c04b2df6c8d28fdf895da828e469baf87523
Linus Mjornstedt <carl.mjornstedt@gmail.com>
2020-11-02 15:31:25 +0000

    Added second test for add function, test passing

--- spec/BankSpec.js
+++ spec/BankSpec.js
@@ -3,4 +3,12 @@ describe("Bank", function() {
         bank = new Bank 
         expect( bank.balance ).toEqual(0);
     }); 
+
+    it("Returns the balance when 1000 has been added to balance", function() {
+        bank = new Bank 
+        bank.add(1000)
+        expect( bank.balance ).toEqual(1000);
+    }); 
+
+
 });
--- src/bank.js
+++ src/bank.js
@@ -2,4 +2,9 @@ class Bank {
     constructor() {
      this.balance = 0;
     }
+    
+     add(number) {
+         this.balance = this.balance + number;
+        return ;
+      }
 }
