180f5b2005f0163b3f741f9472a6c405d7c0f32e
Linus Mjornstedt <carl.mjornstedt@gmail.com>
2020-11-03 17:03:42 +0000

    Added dates to statement array and a function to convert it into correct format. Still needs testing but i don't know how to stub yet.

--- spec/BankSpec.js
+++ spec/BankSpec.js
@@ -19,15 +19,31 @@ describe("Bank", function() {
     it("Bank is initialized with bank statement headers", function() {
         bank = new Bank 
         expect( bank.statement ).toContain("date || credit || debit || balance")
-
     }); 
 
     it("adds the deposit amount as a string to the statement array", function() {
         bank = new Bank 
         bank.deposit(1000)
-        expect( bank.statement ).toContain("1000.00")
-
+        expect( bank.statement[1] ).toContain("1000.00")
     }); 
+
+    it("adds the withdraw amount as a string to the statement array", function() {
+        bank = new Bank 
+        bank.withdraw(500)
+        expect( bank.statement[1] ).toContain(" 500.00 ")
+    }); 
+
+    // it("adds times when deposit was made to deposit string", function() {
+    //     bank = new Bank 
+    //     bank.withdraw(500)
+    //     expect( bank.statement ).toContain("|| || 500.00 || 500.00")
+    // }); 
+
+    // it("adds pipes and current balance to statement array to make the final output match criteria ", function() {
+    //     bank = new Bank 
+    //     bank.withdraw(1000)
+    //     expect( bank.statement ).toContain("1000.00")
+    // }); 
 
 
     
--- src/bank.js
+++ src/bank.js
@@ -4,17 +4,36 @@ class Bank {
      this.statement = ["date || credit || debit || balance"]
 
     }
-    
-     deposit(number) {
-         this.balance = this.balance + number;
 
-         this.statement.push(number.toFixed(2).toString());
-        return this.balance;
+    formatDate(date) {
+        let month = String(date.getMonth() + 1);
+        let day = String(date.getDate());
+        const year = String(date.getFullYear());
+      
+        if (month.length < 2) month = '0' + month;
+        if (day.length < 2) day = '0' + day;
+      
+        return `${day}/${month}/${year}`;
+        }
+
+     deposit(number,date = new Date()) {
+         this.balance = this.balance + number;
+         var formattedDate = this.formatDate(date)
+         date.toString();
+         var depositAmount = number.toFixed(2).toString();
+         var depositFull = formattedDate + " || || " + depositAmount + " || " + this.balance.toFixed(2);
+         this.statement.push(depositFull);
+        return this.balance
       }
 
-     withdraw(number){
-         this.balance = this.balance - number;
-         return this.balance;
+      withdraw(number,date = new Date()) {
+        this.balance = this.balance - number;
+        var formattedDate = this.formatDate(date)
+        date.toString();
+        var withdrawAmount = number.toFixed(2).toString();
+        var withdrawFull = formattedDate + " || " + withdrawAmount + " || || " + this.balance.toFixed(2);
+        this.statement.push(withdrawFull);
+       return this.balance
       }
 
 
