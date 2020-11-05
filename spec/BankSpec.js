8bc98e48f6775a2a143a232b39d0c189e4694ad3
Linus Mjornstedt <carl.mjornstedt@gmail.com>
2020-11-05 14:58:39 +0000

    two more tests written

--- spec/BankSpec.js
+++ spec/BankSpec.js
@@ -53,6 +53,41 @@ describe("Bank", function() {
         expect(bank.fullStatement).toEqual (["date || credit || debit || balance", "12/12/2012 || || 2000.00 || 2000.00", "12/12/2012 || 1000.00 || || 1000.00"])
     }); 
 
+    it("prints the entire statement following a deposit and withdraw ", function() {
+        bank = new Bank 
+        bank.deposit(2000,"12/12/2012")
+        bank.withdraw(1000, '12/12/2012')
+        bank.printStatement()
+        expect(bank.fullStatement).toEqual (["date || credit || debit || balance", "12/12/2012 || || 2000.00 || 2000.00", "12/12/2012 || 1000.00 || || 1000.00"])
+    }); 
+
+    it("makes sure the validate date method returns false when incorrect date format added", function() {
+        bank = new Bank 
+        bank.validateDate("12/12/12")
+        expect( bank.validDate).toEqual(false)
+    }); 
+
+
+    it("makes sure the validate date method return true when correct date format added", function() {
+        bank = new Bank 
+        bank.validateDate("12/12/2012")
+        expect( bank.validDate).toEqual(true)
+    }); 
+
+    it("makes sure the validate date method return false when leap day is added in a non leap year", function() {
+        bank = new Bank 
+        bank.validateDate("29/02/2019")
+        expect( bank.validDate).toEqual(true)
+    }); 
+
+    it("does not add transaction to the statement when wrong date format is added", function() {
+        bank = new Bank 
+        bank.deposit(2000,"12/12/2012")
+        bank.withdraw(1000, '12/12/20')
+        bank.printStatement()
+        expect(bank.fullStatement).toEqual (["date || credit || debit || balance", "12/12/2012 || || 2000.00 || 2000.00"])
+    }); 
+
 
     
 
